b588fae707ff4b60109007e37215e813a5c3e2c0
bayapucg <bayapu.edu004@gmail.com>
2019-08-24 22:28:03 +0530

    commit
    fdas

---
+++ footer.php
@@ -0,0 +1,228 @@
+<footer class="ts-footer">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-lg-10 col-xs-12">
+
+					<div class="footer-menu text-center">
+
+						<ul>
+
+							<li>
+
+								<a href="index.html">Home</a>
+
+							</li>
+
+							<li>
+
+								<a href="about-us.html">About Us</a>
+
+							</li>
+
+                            <li>
+
+								<a href="our-team.html">Our Team</a>
+
+							</li>
+
+                            <li>
+
+								<a href="services.html">Services</a>
+
+							</li>
+
+							<li>
+
+								<a href="donate.html"><img src="images/donate1.gif" style="width:80px;"></a>
+
+							</li>
+
+                            <li>
+
+								<a href="terms-%26-conditions.html">Terms & Conditions</a>
+
+							</li>
+
+                            <!--<li>
+
+								<a href="privacy-policy.html">Privacy Policy</a>
+
+							</li>-->
+
+                              <li>
+
+								<a href="contact.html">Contact Us</a>
+
+							</li>
+
+						</ul>
+
+					</div>
+
+				</div>
+
+                <div class="col-lg-2 col-xs-12">
+
+                
+
+                <ul class="top-social ftsmo">
+
+						<li>
+
+							<a href="https://www.facebook.com/Nice-Scouts-guides-2116347731789451/" target="_blank">
+
+								<i class="fa fa-facebook"></i>
+
+							</a>
+
+                            <a href="https://twitter.com/GuidesNice" target="_blank">
+
+								<i class="fa fa-twitter"></i>
+
+							</a>
+
+							<a href="https://www.instagram.com/nsg.india/" target="_blank">
+
+								<i class="fa fa-instagram"></i>
+
+							</a>
+
+							<a href="https://www.youtube.com/channel/UC2Huu8OS6Jy9fJGWa97T7xg" target="_blank">
+
+								<i class="fa fa-youtube"></i>
+
+							</a>
+
+						</li>
+
+					</ul>
+
+                </div>
+
+			</div>
+
+            <div class="col-md-12">
+
+            <div class="copyright-text text-center">
+
+                     <p>&copy; 2019 Nice Scouts Guides. All Rights Reserved</p>
+
+					</div>
+
+                    </div>
+
+			<div id="back-to-top" class="back-to-top">
+
+				<button class="btn btn-primary" title="Back to Top">
+
+					<i class="fa fa-angle-up"></i>
+
+				</button>
+
+			</div>
+
+            <div class="clearfix"></div>
+
+            <style>
+
+			.vist{border:solid 1px #fff; background-color:#fff;}
+
+			.pd-vs{padding-bottom:0px; padding-top:20px;}
+
+			@media (min-width:320px) and (max-width:767px) 
+
+{.pd-vs{padding-bottom:40px;}}
+
+			</style>
+
+            <div class="col-md-12 text-center pd-vs">
+
+         <p style="font-size:20px; font-weight:bold; color:#fff;"> Visit Counter : 
+
+<img src="https://smallseotools.com/counterDisplay?code=9e01e2b95462328fc10b32b032b3c816&amp;style=0012&amp;pad=9&amp;type=page&amp;initCount=1"  title="Visitor Hit Counter" Alt="Visitor Hit Counter" border="0">
+
+
+
+</p>
+
+                    </div>
+
+			<div id="back-to-top" class="back-to-top">
+
+				<button class="btn btn-primary" title="Back to Top">
+
+					<i class="fa fa-angle-up"></i>
+
+				</button>
+
+			</div>
+
+		</div>
+
+		</div>
+
+	</footer>
+
+     <!------------------------>
+     
+        <div class="socialBtn">
+
+			<ul>
+<li class="dsp-dont"><a href="donate-2.html"><img src="images/donate2.png"></a></li>
+		    	<li><a href="https://www.facebook.com/Nice-Scouts-guides-2116347731789451/" target="_blank" style="background-color: rgb(20, 110, 186);"><i class="fa fa-facebook"></i></a></li>
+
+                 <li><a href="https://twitter.com/GuidesNice" target="_blank" style="background-color: rgb(29, 161, 242);"><i class="fa fa-twitter"></i></a></li>
+
+		        <li><a href="https://www.instagram.com/nsg.india/" target="_blank" style="background-color: rgb(153, 18, 180);"><i class="fa fa-instagram"></i></a></li>
+
+			    <li><a href="https://www.youtube.com/channel/UC2Huu8OS6Jy9fJGWa97T7xg" target="_blank" style="background-color: rgb(223, 44, 38);"><i class="fa fa-youtube"></i></a></li>
+                
+
+               
+
+		    </ul>
+
+		</div>
+        <!------------------------>
+	<script src="js/jquery.min.js"></script>
+
+	<!-- navigation JS -->
+
+	<script src="js/navigation.js"></script>
+
+	<!-- Popper JS -->
+
+	<script src="js/popper.min.js"></script>
+
+	<!-- magnific popup JS -->
+
+	<script src="js/jquery.magnific-popup.min.js"></script>
+
+	<!-- Bootstrap jQuery -->
+
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Owl Carousel -->
+
+	<script src="js/owl-carousel.2.3.0.min.js"></script>
+
+	<!-- slick -->
+
+	<script src="js/slick.min.js"></script>
+
+	<!-- smooth scroling -->
+
+	<script src="js/smoothscroll.html"></script>
+
+	<script src="js/main.js"></script>
+
+    <!---------marq-------->
+
+<script  src="js/index.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,259 @@
+
+<!doctype html>
+
+<html lang="en">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>wisdom scouts</title>
+    <meta name="description" content=""/>
+	<meta name="keywords" content=""/>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/animate.css">
+
+	<link rel="stylesheet" href="css/icofonts.css">
+
+	<link rel="stylesheet" href="css/owlcarousel.min.css">
+
+	<link rel="stylesheet" href="css/slick.css">
+
+	<link rel="stylesheet" href="css/navigation.css">
+
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<link rel="stylesheet" href="css/colors/color-0.css">
+
+	<link rel="stylesheet" href="css/responsive.css">
+
+    <!-------------marq---------->
+
+      <link rel="stylesheet" href="css/style-mrq.css">
+</head>
+<body class="body-color">
+
+	
+
+	
+
+	<header class="header-default">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-lg-2 pr-0">
+
+					<div class="logo">
+
+						<a href="index.html">
+
+							<img src="images/logo.png" alt="" class="logo-sty">
+
+						</a>
+
+					</div>
+
+
+
+				</div>
+
+               
+
+				<div class="col-lg-10 header-nav-item">
+
+					<nav class="navigation ts-main-menu ts-menu-sticky navigation-landscape">
+
+						<div class="nav-header">
+
+							<a class="nav-brand mobile-logo visible-xs" href="index.html">
+
+								<img src="images/logo.png" alt="" class="logo-sty">
+
+							</a>
+
+							<div class="nav-toggle"></div>
+
+						</div>
+
+						<div class="nav-menus-wrapper clearfix">
+
+                            <ul class="align-to-right login-sty">
+
+                             <li><a href="donate.html" class="btn-dnt"><i class="fa fa-heart"></i> Donate</a></li>
+
+
+<li><a href="login.html"><i class="fa fa-user"></i> Register</a></li>
+
+							
+
+							</ul>
+
+<ul class="nav-menu">
+
+<li  class="active" >
+<a href="index.html">Home</a>
+</li>
+
+<li ><a href="about-us.html">About Us</a></li>
+
+<li >
+
+<a href="#">Career</a>
+
+<ul class="nav-dropdown">
+
+<li >
+
+<a href="current-vacancies.html">Current Vacancies</a>
+
+</li>
+
+<li>
+
+<a href="recruitment-system.html">Recruitment System</a>
+
+</li>
+
+</ul>
+
+</li>
+
+<li ><a href="result.html">Result</a></li>
+
+<li >
+
+<a href="#">Circulars</a>
+
+<ul class="nav-dropdown">
+
+<li>
+
+<a href="images/pdf/RTIApplicationForm.pdf" target="_blank">Right to Information</a>
+
+</li>
+
+<li>
+
+<a href="images/pdf/job-application-form.pdf" target="_blank">Job Application Form</a>
+
+</li>
+
+</ul>
+
+</li>
+
+<li >
+
+<a href="gallery.html">Gallery</a>
+
+<ul class="nav-dropdown">
+
+<li>
+
+<a href="photo-gallery.html">Photo Gallery</a>
+
+</li>
+
+<li>
+
+<a href="videos-gallery.html">Videos Gallery</a>
+
+</li>
+
+
+
+</ul>
+
+</li>
+
+<li   >
+
+<a href="services.html">Services</a>
+
+<ul class="nav-dropdown">
+
+<li>
+
+<a href="yoga.html">Yoga</a>
+
+<ul class="nav-dropdown">
+
+<li>
+
+<a href="meditation.html">Meditation</a>
+
+</li>
+
+<li>
+
+<a href="power-yoga.html">Power Yoga</a>
+
+</li>
+
+
+
+</ul>
+
+</li>
+
+<li><a href="self-defense.html">Self Defense</a></li>
+
+<li>
+
+<a href="scout-guide.html">Scout Guide</a>
+
+
+
+</li>
+
+</ul>
+
+</li>
+
+<li ><a href="contact.html">Contact Us</a></li>
+
+</ul>
+
+						</div>
+
+					</nav>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</header>
+<style>
+
+
+.nav-menu > li > a {
+
+height: 60px;
+
+padding: 22px 10px;
+
+}
+
+</style>
+
+				</div>
+
+			</div>
+
+            </div>
+
+            </div>
+
+		</div>
+	</header>
